d462cf9d3936c06c5ddbfc30668d7b997061e61b
Joo-Byungho <kjcoco13@gmail.com>
2024-06-17 19:40:10 +0900

    Feat: Change user find one args field

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,7 +26,7 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne({ id: +id });
   }
 
   @Patch(':id')
