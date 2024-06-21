f46371b7bbee4110cc192bf378df6da98212a0fc
Joo-Byungho <kjcoco13@gmail.com>
2024-06-21 18:13:38 +0900

    Feat: Apply mocks to the spec file with implementing Mikro-orm

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,21 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { MockService, MockServiceFactory } from 'src/common/helper/mock.helper';
+
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let mockedService: MockService<UsersService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useFactory: MockServiceFactory.getMockService(UsersService),
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    mockedService = module.get<MockService<UsersService>>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(mockedService).toBeDefined();
   });
 });
