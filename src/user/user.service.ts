840c216fe639f36086c2c1508eb907856019d68f
Joo-Byungho <kjcoco13@gmail.com>
2024-06-23 17:16:30 +0900

    Chore: Change user module name from plural to singular (#10)

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,34 +7,34 @@ import { wrap } from '@mikro-orm/postgresql';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { User } from './entities/user.entity';
-import { UsersRepository } from './users.repository';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UsersService {
+export class UserService {
   constructor(
-    @InjectRepository(User) private readonly usersRepository: UsersRepository,
+    @InjectRepository(User) private readonly userRepository: UserRepository,
   ) {}
   async create(createUserDto: CreateUserDto) {
-    const user: User = this.usersRepository.create(createUserDto);
-    await this.usersRepository.persistAndFlush(user);
+    const user: User = this.userRepository.create(createUserDto);
+    await this.userRepository.persistAndFlush(user);
     return user;
   }
 
   findAll(): Promise<User[]> {
-    return this.usersRepository.findAll();
+    return this.userRepository.findAll();
   }
 
   findOne(where: FilterQuery<User>): Promise<User> {
-    return this.usersRepository.findOne(where);
+    return this.userRepository.findOne(where);
   }
   async update(id: number, updateUserDto: UpdateUserDto) {
-    const user = await this.usersRepository.findOneOrFail(id);
+    const user = await this.userRepository.findOneOrFail(id);
     wrap(user).assign(updateUserDto);
-    await this.usersRepository.persistAndFlush(user);
+    await this.userRepository.persistAndFlush(user);
     return user;
   }
 
   remove(id: number) {
-    return this.usersRepository.nativeDelete({ id });
+    return this.userRepository.nativeDelete({ id });
   }
 }
