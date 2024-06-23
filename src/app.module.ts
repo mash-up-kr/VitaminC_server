a60dbeef601bbadb5b95d8573496d2628314b571
Joo-Byungho <kjcoco13@gmail.com>
2024-06-23 17:03:43 +0900

    Feat/env validation: env validation 만들었어요 (#9)
    * Feat: Add cache in config module to increase performance
    
    * Feat: Add environment validation
    
    - Install joi
    - Create validate function

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { getEnvPath } from './common/helper/env.helper';
+import { envValidation } from './common/helper/env.validation';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -14,6 +15,8 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: getEnvPath(`${__dirname}/..`),
+      cache: true,
+      validate: envValidation,
     }),
     MikroOrmModule.forRoot(),
     AuthModule,
