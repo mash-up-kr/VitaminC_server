96b1ff46e483849ac5980e5d7a646a4ddef054d2
daisy-kim <sally0226@naver.com>
2024-06-29 22:17:26 +0900

    test: cookie test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -41,7 +41,6 @@ export class AuthController {
       //   this.configService.get('NODE_ENV') === NODE_ENVIRONMENT['production']
       //     ? true
       //     : false,
-      domain: 'localhost',
     });
 
     return res.redirect(302, this.configService.get('CLIENT_URL'));
