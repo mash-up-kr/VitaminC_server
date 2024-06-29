a3a425d45d9e0e5866c880c3f8c06898a28a7916
Daisy <sally0226@naver.com>
2024-06-30 03:32:21 +0900

    Feat: User 관련 API 추가 (#28)
    * Refactor: Edit updateUserDTO
    
    - update하지 않을 필드 제거 (provider 관련)
    - controller용 DTO 분리
        - 컨트롤러에서는 nickname만 업데이트 가능하기 때문
    
    * Feat: impl user API
    
    - 유저 닉네임 업데이트 API
    - 유저 닉네임 중복체크 API
    
    * Chore: Remove unused code

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -1,3 +1,5 @@
+import { HttpStatus } from '@nestjs/common';
+
 import { createException } from './exception.factory';
 import { ExceptionType } from './exception.type';
 
@@ -7,4 +9,12 @@ const ExceptionOf = {
   EXTERNAL: createException(ExceptionType.EXTERNAL),
 };
 
-// export class UserNotFoundException extends ExceptionOf.USER(400, '존재하지 않는 유저입니다.' as const) {}
+export class UserNotFoundException extends ExceptionOf.USER(
+  HttpStatus.NOT_FOUND,
+  '존재하지 않는 유저입니다.' as const,
+) {}
+
+export class DuplicateNicknameException extends ExceptionOf.USER(
+  HttpStatus.CONFLICT,
+  '이미 사용중인 닉네임입니다.' as const,
+) {}
