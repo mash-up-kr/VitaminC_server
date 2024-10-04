90ac2b09845765abc20fee01171937792c18531f
Joo-Byungho <kjcoco13@gmail.com>
2024-10-04 14:16:39 +0900

    Feat: Add fixed

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -52,11 +52,9 @@ export class PlaceService {
    * map id (GroupMap.id)에 속한 장소를 전부 가져옵니다.
    * TODO: 나중에 커지면 geo-query + pagination 해야할듯
    */
-  async getAllPlacesForMap({
-    mapId,
-  }: {
-    mapId: string;
-  }): Promise<PlaceForMapResponseDto[]> {
+  async getAllPlacesForMap(
+    { mapId }: { mapId: string },
+  ): Promise<PlaceForMapResponseDto[]> {
     const placesForMapList = await this.placeForMapRepository.find(
       {
         map: rel(GroupMap, mapId),
@@ -122,20 +120,22 @@ export class PlaceService {
       (iLike.filter((v) => youLike.some((k) => k.place === v.place)).length /
         iLike.length) *
       100
-    );
+    ).toFixed(1);
   }
 
-  async registerPlaceByKakaoId({
-    kakaoPlaceId,
-    mapId,
-    user,
-    registerPlaceDto,
-  }: {
-    kakaoPlaceId: number;
-    mapId: string;
-    user: User;
-    registerPlaceDto: RegisterPlaceDto;
-  }) {
+  async registerPlaceByKakaoId(
+    {
+      kakaoPlaceId,
+      mapId,
+      user,
+      registerPlaceDto,
+    }: {
+      kakaoPlaceId: number;
+      mapId: string;
+      user: User;
+      registerPlaceDto: RegisterPlaceDto;
+    },
+  ) {
     let place = await this.placeRepository.findOne({
       kakaoPlace: rel(KakaoPlace, kakaoPlaceId),
     });
@@ -245,17 +245,19 @@ export class PlaceService {
     return new PlaceResponseDto(place as unknown as PlaceForMap);
   }
 
-  async likePlace({
-    mapId,
-    placeId,
-    user,
-    like,
-  }: {
-    mapId: string;
-    placeId: number;
-    user: User;
-    like: boolean;
-  }) {
+  async likePlace(
+    {
+      mapId,
+      placeId,
+      user,
+      like,
+    }: {
+      mapId: string;
+      placeId: number;
+      user: User;
+      like: boolean;
+    },
+  ) {
     const placeForMap = await this.placeForMapRepository.findOneOrFail(
       {
         place: rel(Place, placeId),
