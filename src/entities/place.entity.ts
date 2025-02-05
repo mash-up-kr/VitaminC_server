2a506e4eb559c9bc67517e140387fe1efd061dd4
Joo-Byungho <kjcoco13@gmail.com>
2025-02-05 14:51:17 +0900

    Feat: Add location geometry to place entity and migration for PostGIS support
    - Enable PostGIS
    - Add gist index of place location column

--- src/entities/place.entity.ts
+++ src/entities/place.entity.ts
@@ -1,4 +1,6 @@
 import {
+  BeforeCreate,
+  BeforeUpdate,
   Entity,
   EntityRepositoryType,
   OneToOne,
@@ -7,6 +9,7 @@ import {
 } from '@mikro-orm/core';
 
 import { KakaoPlace } from './kakao-place.entity';
+import { Point, PointType } from './place.point';
 import { PlaceRepository } from './place.repository';
 
 @Entity({
@@ -22,6 +25,9 @@ export class Place {
 
   @OneToOne({ entity: () => KakaoPlace })
   kakaoPlace: KakaoPlace;
+
+  @Property({ type: PointType, nullable: true })
+  location: Point | null;
 
   @Property({
     type: 'double precision',
@@ -44,4 +50,10 @@ export class Place {
   updatedAt: Date = new Date();
 
   [EntityRepositoryType]: PlaceRepository;
+
+  @BeforeCreate()
+  @BeforeUpdate()
+  setLocationBeforeSave() {
+    this.location = new Point(this.x, this.y);
+  }
 }
---
+++ src/entities/place.point.ts
@@ -0,0 +1,40 @@
+import { Type } from '@mikro-orm/core';
+
+export class Point {
+  constructor(
+    public latitude: number,
+    public longitude: number,
+  ) {}
+}
+
+export class PointType extends Type<Point | undefined, string | undefined> {
+  convertToDatabaseValue(value: Point | undefined): string | undefined {
+    if (!value) {
+      return;
+    }
+
+    return `point(${value.latitude} ${value.longitude})`;
+  }
+
+  convertToJSValue(value: string | undefined): Point | undefined {
+    const m = value?.match(/point\((-?\d+(\.\d+)?) (-?\d+(\.\d+)?)\)/i);
+
+    if (!m) {
+      return undefined;
+    }
+
+    return new Point(+m[1], +m[3]);
+  }
+
+  convertToJSValueSQL(key: string) {
+    return `ST_AsText(${key})`;
+  }
+
+  convertToDatabaseValueSQL(key: string) {
+    return `ST_PointFromText(${key})`;
+  }
+
+  getColumnType(): string {
+    return 'geometry';
+  }
+}
